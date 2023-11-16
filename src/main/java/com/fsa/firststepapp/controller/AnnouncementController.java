8609642cefedcecb0cde2b1c86033fc1d2a7ad50
Daniel Iuga <danieliuga33@yahoo.com>
2023-11-16 22:27:58 +0200

    Added filter Anoncement Controller functions

--- src/main/java/com/fsa/firststepapp/controller/AnnouncementController.java
+++ src/main/java/com/fsa/firststepapp/controller/AnnouncementController.java
@@ -3,10 +3,7 @@ package com.fsa.firststepapp.controller;
 import com.fsa.firststepapp.models.dto.AnnouncementDto;
 import com.fsa.firststepapp.service.announcement_service.IAnnouncementService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,15 @@ public class AnnouncementController {
     @GetMapping("")
     public List<AnnouncementDto> getAllAnnouncements() {
         return announcementService.getAllAnnouncements();
+    }
+
+    @GetMapping("/filterCategory")
+    public List<AnnouncementDto> getAnnouncementsByCategory(@RequestParam String category){
+        return announcementService.getAnnouncementsByCategory(category);
+    }
+
+    @GetMapping("/filterUniversity")
+    public List<AnnouncementDto> getAnnouncementsByUniversity(@RequestParam String university){
+        return announcementService.getAnnouncementsByUniversity(university);
     }
 }
