e999769b129c0420cc19ad506ad7e931a197e3e6
andrey100f <andreiforminte19@yahoo.com>
2023-11-16 21:52:40 +0200

    Fixed typos

---
+++ src/main/java/com/fsa/firststepapp/controller/LocationController.java
@@ -0,0 +1,28 @@
+package com.fsa.firststepapp.controller;
+
+import com.fsa.firststepapp.models.dto.LocationDto;
+import com.fsa.firststepapp.service.location_service.ILocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins="*", allowedHeaders="*")
+@RequestMapping("/api/locations")
+public class LocationController {
+    private final ILocationService locationService;
+
+    @Autowired
+    public LocationController(ILocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping("")
+    public List<LocationDto> getAllLocations() {
+        return locationService.getAllLocations();
+    }
+}
