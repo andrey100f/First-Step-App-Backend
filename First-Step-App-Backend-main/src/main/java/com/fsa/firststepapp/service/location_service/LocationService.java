5155a677a48235618b3e9c828611ec5f221afcd9
ghrsmg <91556142+ghrsmg@users.noreply.github.com>
2023-11-16 16:29:18 +0200

    Add files via upload

---
+++ First-Step-App-Backend-main/src/main/java/com/fsa/firststepapp/service/location_service/LocationService.java
@@ -0,0 +1,36 @@
+package com.fsa.firststepapp.service.location_service;
+
+import com.fsa.firststepapp.models.Location;
+import com.fsa.firststepapp.models.dto.LocationDto;
+import com.fsa.firststepapp.models.exception.models.EntityNotFoundException;
+import com.fsa.firststepapp.models.mappers.LocationMapper;
+import com.fsa.firststepapp.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LocationService implements ILocationService {
+    private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
+
+    @Autowired
+    public LocationService(LocationRepository locationRepository, LocationMapper locationMapper) {
+        this.locationRepository = locationRepository;
+        this.locationMapper = locationMapper;
+    }
+
+    @Override
+    public List<LocationDto> getAllLocations() {
+        List<Location> locations = (List<Location>) locationRepository.findAll();
+
+        if (locations.isEmpty()) {
+            throw new EntityNotFoundException("Locations Not Found!!");
+        }
+
+        return locationMapper.convertModelListToDtoList(locations);
+    }
+}
