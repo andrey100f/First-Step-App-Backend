2edc1acbfe29d2b4403b8df83714ccc98f1f7227
ghrsmg <91556142+ghrsmg@users.noreply.github.com>
2023-11-18 13:38:41 +0200

    Add files via upload

--- src/main/java/com/fsa/firststepapp/repository/EventRepository.java
+++ src/main/java/com/fsa/firststepapp/repository/EventRepository.java
@@ -1,14 +1,13 @@
 package com.fsa.firststepapp.repository;
 
+import com.fsa.firststepapp.models.Event;
 import com.fsa.firststepapp.models.Location;
-import com.fsa.firststepapp.models.dto.LocationDto;
+import com.fsa.firststepapp.models.dto.EventDto;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 @Repository
-public interface LocationRepository extends CrudRepository<Location, Long> {
-    List<Location> getLocationsByType(String type);
-
+public interface EventRepository extends CrudRepository<Location, Long> {
+    List<Event> getEventsByLocationID(Long id);
 }
---
+++ src/main/java/com/fsa/firststepapp/repository/UniversityRepository.java
@@ -0,0 +1,14 @@
+package com.fsa.firststepapp.repository;
+
+import com.fsa.firststepapp.models.University;
+import com.fsa.firststepapp.models.dto.UniversityDto;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UniversityRepository extends CrudRepository<University, Long> {
+
+
+}
