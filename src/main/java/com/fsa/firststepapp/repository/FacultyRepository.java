4df6f5f0087ad03d36e0b9d4173bdb3a53e51d88
AlexandruGirlea01 <alexandrugirlea36@gmail.com>
2023-11-22 17:15:54 +0200

    Modified register endpoint + added CORS

--- src/main/java/com/fsa/firststepapp/repository/FacultyRepository.java
+++ src/main/java/com/fsa/firststepapp/repository/FacultyRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface FacultyRepository extends CrudRepository<Faculty, Long> {
     Faculty findFacultyByFacultyId(Long id);
+    Faculty findFacultyByName(String name);
 }
