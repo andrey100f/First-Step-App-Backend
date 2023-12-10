b330fb867ad8292e24ef8303397045aa40b320c6
andrey100f <andreiforminte19@yahoo.com>
2023-12-10 09:40:48 +0200

    Fixed the service parameters

--- src/main/java/com/fsa/firststepapp/models/request/AddAnswerRequest.java
+++ src/main/java/com/fsa/firststepapp/models/request/AddAnswerRequest.java
@@ -5,15 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddResponseRequest {
+public class AddAnswerRequest {
     private String text;
-    private Date answerDate;
     private String user;
     private Long question;
 }
--- src/main/java/com/fsa/firststepapp/service/question_service/IQuestionService.java
+++ src/main/java/com/fsa/firststepapp/service/question_service/IQuestionService.java
@@ -1,10 +1,11 @@
 package com.fsa.firststepapp.service.question_service;
 
 import com.fsa.firststepapp.models.dto.QuestionDto;
+import com.fsa.firststepapp.models.request.AddQuestionRequest;
 
 import java.util.List;
 
 public interface IQuestionService {
     List<QuestionDto> getAllQuestions();
-    QuestionDto addQuestion(String userEmail, String questionText, String category);
+    QuestionDto addQuestion(AddQuestionRequest addQuestionRequest);
 }
