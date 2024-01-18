986e2646a163861cca11a124e5e3fb5b0c883c38
Giosan Stefana <stefana.giosan@gmail.com>
2024-01-18 13:01:06 +0200

    Added comments in config.

--- src/main/java/com/fsa/firststepapp/config/JwtAuthenticationFilter.java
+++ src/main/java/com/fsa/firststepapp/config/JwtAuthenticationFilter.java
@@ -17,17 +17,32 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * Filtru pentru gestionarea autentificării utilizând JSON Web Token (JWT).
+ */
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
+    /**
+     * Implementarea metodei principale a filtrului pentru gestionarea autentificării prin JWT.
+     *
+     * @param request       Cererea HTTP.
+     * @param response      Răspunsul HTTP.
+     * @param filterChain   Lanțul de filtre.
+     * @throws ServletException Excepție în cazul unor erori de servlet.
+     * @throws IOException      Excepție în cazul unor erori de IO.
+     */
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        // Se exclude filtrul pentru căile de autentificare
         if (request.getServletPath().contains("/api/auth")) {
             filterChain.doFilter(request, response);
             return;
@@ -35,14 +50,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
+
+        // Se exclude filtrul dacă header-ul Authorization lipsește sau nu începe cu "Bearer "
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
             return;
         }
+
+        // Extragerea token-ului JWT
         jwt = authHeader.substring(7);
+
+        // Verificarea și validarea token-ului JWT
         userEmail = jwtService.extractUsername(jwt);
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+            // Crearea și setarea autentificării în contextul de securitate
             if(jwtService.isTokenValid(jwt, userDetails)){
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
@@ -53,6 +76,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+
+        // Propagarea cererii în lanțul de filtre
         filterChain.doFilter(request, response);
     }
 }
